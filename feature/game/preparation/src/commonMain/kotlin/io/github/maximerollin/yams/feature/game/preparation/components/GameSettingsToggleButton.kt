add441d8ee7928e43cdedb92cf720853ca1a0da2
Maxime Rollin <m.rollin@akawan.fr>
2026-02-03 17:27:25 +0100

    Enhance GameSettingsToggleButton with minimum height for improved UI consistency
    - Added a minimum height constraint to the GameSettingsToggleButton component to ensure consistent sizing across different UI states.
    - This adjustment enhances the overall user experience by providing a more uniform appearance in the game settings interface.

--- feature/game/preparation/src/commonMain/kotlin/io/github/maximerollin/yams/feature/game/preparation/components/GameSettingsToggleButton.kt
+++ feature/game/preparation/src/commonMain/kotlin/io/github/maximerollin/yams/feature/game/preparation/components/GameSettingsToggleButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -44,6 +45,7 @@ public fun GameSettingsToggleButton(
 
     Surface(
         modifier = modifier
+            .heightIn(min = 36.dp)
             .clip(shape)
             .clickable(onClick = onClick),
         shape = shape,
