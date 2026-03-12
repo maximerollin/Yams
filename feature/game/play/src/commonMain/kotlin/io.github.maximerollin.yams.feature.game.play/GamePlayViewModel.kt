fa6575c303f52e3310ef728a77476d5c07672083
Maxime Rollin <m.rollin@akawan.fr>
2026-03-12 10:39:59 +0100

    Implement GamePlayViewModel with score entry and game state management
    - Added GamePlayViewModel to manage game play state and score entries.
    - Integrated score entry functionality with the ScoreEntryRepository for creating and deleting score entries.
    - Implemented methods for handling game finishing and navigation to game results.
    - Utilized StateFlow for reactive game state management, enhancing UI responsiveness.

--- feature/game/play/src/commonMain/kotlin/io.github.maximerollin.yams.feature.game.play/GamePlayViewModel.kt
+++ feature/game/play/src/commonMain/kotlin/io.github.maximerollin.yams.feature.game.play/GamePlayViewModel.kt
@@ -1,9 +1,69 @@
 package io.github.maximerollin.yams.feature.game.play
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.maximerollin.yams.core.model.GameId
+import io.github.maximerollin.yams.data.game.ScoreEntryRepository
+import io.github.maximerollin.yams.data.game.model.CreateScoreEntry
+import io.github.maximerollin.yams.data.game.model.ScoreCellRef
+import io.github.maximerollin.yams.feature.game.play.domain.FinishGameUseCase
+import io.github.maximerollin.yams.feature.game.play.domain.GetGamePlayStateUseCase
+import io.github.maximerollin.yams.feature.game.play.model.GamePlayStateUi
+import io.github.maximerollin.yams.feature.game.play.model.GameStatus
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import org.koin.core.annotation.InjectedParam
 
 internal class GamePlayViewModel(
-
+    @InjectedParam private val gameId: GameId,
+    private val scoreEntryRepository: ScoreEntryRepository,
+    private val finishGameUseCase: FinishGameUseCase,
+    private val getGetGamePlayStateUseCase: GetGamePlayStateUseCase,
 ) : ViewModel() {
 
+    val gamePlayStateUi: StateFlow<GamePlayStateUi?> =
+        getGetGamePlayStateUseCase(gameId)
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = null,
+            )
+
+    private val _navigateToGameResult: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val navigateToGameResult: StateFlow<Boolean> = _navigateToGameResult
+
+    fun onScore(score: Int, cell: ScoreCellRef) {
+        if (gamePlayStateUi.value?.status != GameStatus.ONGOING) return
+
+        viewModelScope.launch {
+            gamePlayStateUi.value?.let { state ->
+                scoreEntryRepository.createScoreEntry(
+                    value = CreateScoreEntry(
+                        gameId = gameId,
+                        userId = state.currentPlayer.userId,
+                        cell = cell,
+                        score = score,
+                    )
+                )
+            }
+        }
+    }
+
+    fun onUndo() {
+        viewModelScope.launch {
+            scoreEntryRepository.deleteLastScoreEntryOfGame(gameId)
+        }
+    }
+
+    fun onFinishGame() {
+        viewModelScope.launch {
+            gamePlayStateUi.value?.let { state ->
+                finishGameUseCase(gameId, state)
+                _navigateToGameResult.value = true
+            }
+        }
+    }
 }
